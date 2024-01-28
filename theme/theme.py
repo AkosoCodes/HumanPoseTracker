d6b4b2c1ba6ce554bc4340d8f2977c1b926dcf95
Andrija <andrijas0721@gmail.com>
2024-01-28 19:57:24 +0100

    🖼️~ Added and updated colors

--- theme/theme.py
+++ theme/theme.py
@@ -1,6 +1,8 @@
-statusBox = (245, 117, 16)
+statusBox = (50, 50, 50, 150)
 statusLabel = (255,255,255)
-statusValue = (0,0,0)
+statusValue = (221,160,221)
+
+repColor = (0, 165, 255)
 
 lineColor = (153,50,204)
 pointColor= (221,160,221)
